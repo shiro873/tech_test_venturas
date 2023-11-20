05ace295cf58bf16831b988e9c3c7d3271211e90
Ridwan Shuvro <shuvro_r@hotmail.com>
2023-11-20 16:15:46 +0600

    add url

---
+++ src/constants/url.js
@@ -0,0 +1,10 @@
+export const PROTOCOL_HOST = 'http://localhost:3001';
+
+export const registerUrl = PROTOCOL_HOST + '/user/signup';
+export const loginUrl = PROTOCOL_HOST + '/user/login';
+export const followUserUrl = PROTOCOL_HOST + '/user/follow';
+export const unfOllowUserUrl = PROTOCOL_HOST + '/user/unfollow';
+export const getFolloweesUrl = PROTOCOL_HOST + '/user/followees';
+export const getFollowersUrl = PROTOCOL_HOST + '/user/followers';
+export const murmurUrl = PROTOCOL_HOST + '/murmur';
+export const likeMurmurUrl = PROTOCOL_HOST + '/murmur/like';
